4c70fbc6d17928c44fea875aee035830dcae39ca
Wagih-13 <wagiha498@gmail.com>
2024-02-04 01:58:28 +0200

    first upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LayOut from "./LayOut/LayOut";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Portfolio from "./Portfolio/Portfolio";
+import Contact from "./Contact/Contact";
 
 function App() {
+  let routers = createBrowserRouter([
+    {
+      path: "",
+      element: <LayOut />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "portfolio", element: <Portfolio /> },
+        { path: "contact", element: <Contact /> },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={routers} />
+    </>
   );
 }
 
